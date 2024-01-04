f6959226cdbeb4067035fc4c08d6748e637d653c
Dmytro <dmytrosablin@gmail.com>
2024-01-04 18:41:37 +0200

    ton

--- server/public/flappybird.js
+++ server/public/flappybird.js
@@ -64,7 +64,7 @@ window.onload = function() {
 
     //load images
     birdImg = new Image();
-    birdImg.src = "./flappybird.png";
+    birdImg.src = "./ton.png";
     birdImg.onload = function() {
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
